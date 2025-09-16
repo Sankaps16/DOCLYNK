7e1cd02f9553c33c29776b7ff58ededf463d3db5
firstuse_repo <sanyakapoorlko@gmail.com>
2025-09-16 20:04:02 +0530

    Added admin authentication with JWT and authAdmin middleware

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import {v2 as cloudinary} from "cloudinary"
 import doctorModel from "../models/doctorModel.js"
+import jwt from 'jsonwebtoken'
 //API for adding doctor 
 const addDoctor = async(req,res) =>{ 
     try{ 
@@ -28,7 +29,7 @@ const addDoctor = async(req,res) =>{
 
         }
         // hashing doctor password
-        const salt =await bcrypt.getSalt(10)//greater no. menaing more secure but slow
+        const salt =await bcrypt.genSalt(10)//greater no. menaing more secure but slow
         const hashedPassword = await bcrypt.hash(password,salt)
         //upload image to cloudinary
         const imageUpload = await cloudinary.uploader.upload(imageFile.path,{resource_type:"image"})
@@ -62,4 +63,30 @@ catch(error){
     res.json({success:false,message:error.message})
 
 } } 
-export {addDoctor}
+
+// API for the admin login
+const loginAdmin = async (req,res) =>{
+    try{
+
+        const {email,password} =  req.body
+
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password,process.env.JWT_SECRET)
+            res.json({success:true,token})
+
+
+        }else{
+            res.json({success:false,message:"Invalid credentials"})
+        }
+
+        
+
+
+    }catch(error){
+        console.log(error)
+    res.json({success:false,message:error.message})
+
+    }
+}
+
+export {addDoctor,loginAdmin}
